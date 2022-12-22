6f0e2f515594513712659324b8c074ce56e7d3fb
NurhajiMuhamad <120303770+NurhajiMuhamad@users.noreply.github.com>
2022-12-22 22:38:31 +0900

    kedua

--- app/src/main/java/com/example/binarkuemen2/MainActivity.kt
+++ app/src/main/java/com/example/binarkuemen2/MainActivity.kt
@@ -9,5 +9,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         println("jumlah")
+        println("kurang")
+        //git push -u origin main
     }
 }
