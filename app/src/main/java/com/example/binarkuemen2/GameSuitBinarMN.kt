ff7d900a0ec606e3afec2bfb8ef7e1881621d112
NurhajiMuhamad <120303770+NurhajiMuhamad@users.noreply.github.com>
2022-12-26 12:41:48 +0900

    sui3

--- app/src/main/java/com/example/binarkuemen2/GameSuitBinarMN.kt
+++ app/src/main/java/com/example/binarkuemen2/GameSuitBinarMN.kt
@@ -1,4 +1,37 @@
 package com.example.binarkuemen2
 
 class GameSuitBinarMN {
+    var println("Hello World!")
+    println("=============================")
+    println("GAME SUIT TERMINAL VERSION")
+    println("=============================")
+    println("1. Masukkan pemain 1: ")
+    val pemain1: String = readln()
+    println("=============================")
+    println("2. Masukkan pemain 2: ")
+    val pemain2: String = readln()
+    println("=============================")
+    println("Hasil:")
+    if (pemain1 == "gunting" && pemain2 == "gunting") {
+        println("DRAW!")
+    } else if (pemain1 == "batu" && pemain2 == "batu") {
+        println("DRAW!")
+    } else if (pemain1 == "kertas" && pemain2 == "kertas") {
+        println("DRAW!")
+    } else if (pemain1 == "gunting" && pemain2 == "kertas") {
+        println("Pemain 1 MENANG!")
+    } else if (pemain1 == "gunting" && pemain2 == "batu") {
+        println("Pemain 2 MENANG!")
+    } else if (pemain1 == "batu" && pemain2 == "kertas") {
+        println("Pemain 2 MENANG!")
+    } else if (pemain1 == "batu" && pemain2 == "gunting") {
+        println("Pemain 1 MENANG!") //salah
+    } else if (pemain1 == "kertas" && pemain2 == "gunting") {
+        println("Pemain 2 MENANG!")
+    } else if (pemain1 == "kertas" && pemain2 == "batu") {
+        println("Pemain 1 MENANG!")
+    } else {
+        println("error")
+    }
+    //oke
 }
